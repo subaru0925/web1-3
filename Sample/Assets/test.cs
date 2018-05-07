8bd31d372865f30484c1add0d77573f4a44e22d3
Unknown <adgjmptw.925@icloud.com>
2018-05-07 17:54:52 +0900

    最初2のコミット

--- Sample/Assets/test.cs
+++ Sample/Assets/test.cs
@@ -2,10 +2,45 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public class player
+{
+    private int hp = 100;
+    private int power = 50;
+
+    public void Attack()
+    {
+        Debug.Log(this.power + "のダメージを与えた");
+    }
+    public void Damage(int damage) 
+    {
+        Debug.Log(this.power + damage);
+        {
+            this.hp -= damage;
+            Debug.Log(damage + "のダメージを与えた");
+        }
+    }
+}
+
 public class test : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void sayHello()
+    {
+        Debug.Log("Hello");
+    }
+
+    void CallName(string name)
+    {
+        Debug.Log("Hello" + name);
+    }
+
+    int Add(int a,int b)
+    {
+        int c = a + b;
+        return c;
+    }
+
+    void Start () {
         //Hello,World
         Debug.Log("Hello,World");
 
@@ -117,7 +152,53 @@ public class test : MonoBehaviour {
         }
         
             Debug.Log(sum);
+
+        int[] array = new int[5];
+        array[0] = 2;
+        array[1] = 10;
+        array[2] = 5;
+        array[3] = 15;
+        array[4] = 3;
         
+        for(int i = 0; i < 5; i++)
+        {
+            Debug.Log(array[i]);
+        }
+
+        int[] points = { 83, 99, 52, 93, 15 };
+
+        for(int i = 0; i < points.Length; i++)
+        {
+            if (points[i] >= 90) {
+                Debug.Log(points[i]);
+            }
+        }
+
+        sayHello();
+
+        CallName("Tom");
+
+        answer = Add(2, 3);
+        Debug.Log(answer);
+
+        player myPlayer = new player();
+        myPlayer.Attack();
+        myPlayer.Damage(30);
+
+        Vector2 playerPos = new Vector2(3.0f, 4.0f);
+        playerPos.x += 8.0f;
+        playerPos.y += 5.0f;
+        Debug.Log(playerPos);
+
+        Vector2 startPos = new Vector2(2.0f, 1.0f);
+        Vector2 endPos = new Vector2(8.0f, 5.0f);
+        Vector2 dir = endPos - startPos;
+        Debug.Log(dir);
+
+        float len = dir.magnitude;
+        Debug.Log(len);
+
+
     }
 	
 	// Update is called once per frame
